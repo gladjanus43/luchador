78177f53e352b197bbf392ea5f797e788001275f
Gijs Bijvoets <0950640@hr.nl>
2018-05-23 11:59:29 +0200

    0.0.1
    Added game constructor

--- game/typescript/game.ts
+++ game/typescript/game.ts
@@ -1,7 +1,23 @@
+let game : Phaser.Game
 class Game{
     constructor(){
+        game = new Phaser.Game(800, 600, Phaser.AUTO,'content',{
+            create  : this.create,
+            preload : this.preload,
+            update  : this.update
+        });
+    }
+
+    preload(){
+
+    }
+    create(){
         
     }
+    update(){
+
+    }
+
 }
 window.onload = () => {
     new Game();
