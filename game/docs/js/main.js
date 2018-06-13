c5aeff8109c7034f1bc630f1f7134536c480d64d
Gijs Bijvoets <0950640@hr.nl>
2018-06-13 13:21:16 +0200

    Completed Code for speech bubble
    You can now create a bubble in every phaser class

--- game/docs/js/main.js
+++ game/docs/js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 var game;
+var sayings;
 var Game = (function () {
     function Game() {
         game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
@@ -7,12 +8,26 @@ var Game = (function () {
             preload: this.preload,
             update: this.update
         });
+        sayings = [];
+        this.loadSayings();
     }
+    Game.prototype.loadSayings = function () {
+        var _this = this;
+        fetch("http://localhost/Luchador/luchador/game/docs/connection.php")
+            .then(function (res) { return res.json(); })
+            .then(function (res) { return _this.pushSayings(res); });
+    };
+    Game.prototype.pushSayings = function (dbResults) {
+        console.log("klaar met laden");
+        dbResults.forEach(function (saying) {
+            sayings.push(saying);
+        });
+    };
     Game.prototype.preload = function () {
         console.log('preload');
     };
     Game.prototype.create = function () {
-        var bubble = new Spreekwoord(200, 200);
+        new Spreekwoord(200, 200);
     };
     Game.prototype.update = function () {
     };
@@ -25,10 +40,16 @@ var Spreekwoord = (function () {
     function Spreekwoord(x, y) {
         this.posX = x;
         this.posY = y;
-        this.sayings = [];
+        this.questions = [];
+        this.currentSayings = [];
+        this.correctAnswer = this.randomInt(0, 3);
         this.element = document.createElement("bubble");
-        this.button = document.createElement("button");
-        this.loadSayings();
+        this.span = document.createElement("span");
+        this.button1 = document.createElement("button");
+        this.button2 = document.createElement("button");
+        this.button3 = document.createElement("button");
+        this.createArray();
+        this.createBubble();
     }
     Spreekwoord.prototype.loadSayings = function () {
         var _this = this;
@@ -37,25 +58,53 @@ var Spreekwoord = (function () {
             .then(function (res) { return _this.pushSayings(res); });
     };
     Spreekwoord.prototype.pushSayings = function (dbResults) {
-        var _this = this;
         console.log("klaar met laden");
         dbResults.forEach(function (saying) {
-            _this.sayings.push(saying);
+            sayings.push(saying);
         });
-        this.createBubble();
     };
     Spreekwoord.prototype.createBubble = function () {
-        var bubble = document.getElementById('content');
-        bubble.appendChild(this.element);
+        this.element.addEventListener('click', this.changeInnerText.bind(this));
+        var gameCanvas = document.getElementById('content');
+        gameCanvas.appendChild(this.element);
+        this.element.appendChild(this.span);
         this.element.style.left = this.posX + "px";
         this.element.style.top = this.posY + "px";
-        this.element.innerHTML = this.sayings[0].spreekwoord;
-        this.element.appendChild(this.button);
-        this.button.innerHTML = "Volgende Gezegde";
-        this.button.addEventListener('click', this.clickSuccesHandler);
+        this.element.appendChild(this.button1);
+        this.element.appendChild(this.button2);
+        this.element.appendChild(this.button3);
     };
-    Spreekwoord.prototype.clickSuccesHandler = function () {
-        console.log('Je hebt het goed geraden!');
+    Spreekwoord.prototype.changeInnerText = function (e) {
+        if (e.target.nodeName == "BUTTON") {
+            if (this.currentSayings[this.correctAnswer].antwoord == e.target.innerHTML) {
+                this.createArray();
+            }
+        }
+    };
+    Spreekwoord.prototype.createArray = function () {
+        this.correctAnswer = this.randomInt(0, 3);
+        this.questions = [];
+        this.currentSayings = [];
+        while (this.questions.length < 3) {
+            this.ran = this.randomInt(0, sayings.length);
+            if (this.questions.indexOf(this.ran) == -1) {
+                this.questions.push(this.ran);
+            }
+        }
+        for (var i = 0; i < 3; i++) {
+            this.currentSayings.push(sayings[this.questions[i]]);
+        }
+        this.fillButtons();
+    };
+    Spreekwoord.prototype.fillButtons = function () {
+        this.span.innerHTML = this.currentSayings[this.correctAnswer].spreekwoord;
+        this.button1.innerHTML = this.currentSayings[0].antwoord;
+        this.button2.innerHTML = this.currentSayings[1].antwoord;
+        this.button3.innerHTML = this.currentSayings[2].antwoord;
+    };
+    Spreekwoord.prototype.randomInt = function (begin, end) {
+        var ran = Math.floor(Math.random() * end + begin);
+        return ran;
     };
     return Spreekwoord;
 }());
--- game/typescript/game.ts
+++ game/typescript/game.ts
@@ -1,17 +1,30 @@
 let game : Phaser.Game
-
+let sayings : Saying[];
 class Game{
     /**
      * constructor for the game
      */
     constructor(){
-        
         game = new Phaser.Game(800, 600, Phaser.AUTO,'content',{
             create  : this.create,
             preload : this.preload,
             update  : this.update
         });
-        
+        sayings = []
+        this.loadSayings();
+    }
+
+    loadSayings(){
+        fetch("http://localhost/Luchador/luchador/game/docs/connection.php")
+            .then(res => res.json())
+            .then(res => this.pushSayings(res))
+    }
+
+    pushSayings(dbResults:any){
+        console.log("klaar met laden");
+        dbResults.forEach((saying : any) => {
+            sayings.push(saying)
+        });
     }
 
     /**
@@ -26,7 +39,7 @@ class Game{
      * here they get actually placed in the game
      */
     create(){
-        let bubble = new Spreekwoord(200,200);
+        new Spreekwoord(200,200)
     }
 
     /**
