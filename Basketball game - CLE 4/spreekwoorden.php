4589ef58f29a1b2c728d4d8aaeb63d57b9782abb
DidiZhou25 <didizhou25@hotmail.com>
2018-06-25 19:12:34 +0200

    Add files via upload

---
+++ Basketball game - CLE 4/spreekwoorden.php
@@ -0,0 +1,50 @@
+<?php
+$query = "INSERT INTO antwoord(`desc`)
+VALUES
+("Nu blijkt wat werkelijk de bedoeling was"),
+("Als een karwei samen wordt opgepakt is het snel en gemakkelijk gedaan"),
+("Men mag niet klagen over de kwaliteit van iets dat men gratis krijgt"),
+("Een goed begin vergroot de kans op een goede afwerking"),
+("De details erbij zetten - orde op zaken stellen"),
+("Iemand voor de gek houden"),
+("Een afwachtende houding aannemen"),
+("De loop van het verhaal niet meer kunnen volgen"),
+("Een slecht humeur hebben"),
+("Dat kan er niet mee door"),
+("Als je iets doms doet, moet je de gevolgen dragen"),
+("Uitbundig vieren"),
+("Iets wat niet te doorgronden is"),
+("In een hoge positie heeft men ook veel verantwoordelijkheid"),
+("Hij krijgt wat hij verdient"),
+("Betaling bij de levering"),
+("Dingen zeggen om een goede indruk achter te laten bij mensen met invloed"),
+("Van mensen in zijn omgeving kan men meer hulp verwachten"),
+("Meteen ter zake komen"),
+("Iets verzinnen"),
+("Enthousiast zijn over iets, blij zijn met iets")"
+
+$query = "INSERT INTO spreekwoord(`desc`, ant_id)
+VALUES
+("De aap komt ui de mouw", "12"),
+("Vele handen maken licht werk","13"),
+("men mag een gegeven paard niet in de bek kijken", "14"),
+("Een goed begin is het halve werk", "15"),
+("De puntjes op de i zetten", "16"),
+("Iemand in het ootje nemen", "17"),
+("De kat uit de boom kijken", "18"),
+("De draad kwijt zijn.", "19"),
+("Met het verkeerde been uit bed stappen", "20"),
+("Dat kan niet door de beugel", "21"),
+("Wie zijn billen brandt moet op de blaren zitten", "22")
+("De bloemetjes buiten zetten", "23")
+("Een gesloten boek zijn", "24")
+("Hoge bomen vangen veel wind", "25")
+("Boontje komt om zijn loontje", "26")
+("Boter bij de vis", "27")
+("Zoete broodjes bakken", "28")
+("Een goede buur is beter dan een verre vriend", "29")
+("Met de deur in huis vallen", "30")
+("Uit de duim zuigen", "31")
+("Een gat in de lucht springen", "32")"
+
+ ?>
